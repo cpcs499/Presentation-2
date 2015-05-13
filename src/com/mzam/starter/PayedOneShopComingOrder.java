8c300d0ff9ff33e97cfc2a43404fd56cc9e2c814
cpcs499 <cpcs498.499@gmail.com>
2015-05-13 14:34:57 +0300

    Update PayedOneShopComingOrder.java

--- src/com/mzam/starter/PayedOneShopComingOrder.java
+++ src/com/mzam/starter/PayedOneShopComingOrder.java
@@ -1,23 +1,11 @@
 package com.mzam.starter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -25,11 +13,33 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.database.CursorJoiner.Result;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
 public class PayedOneShopComingOrder extends Activity {
 
 	ListView listView ;
 	ImageView userpic;
-	List<String> obList ,obList2,obList3,obList4;
+	List<String> obList ,obList2,obList3,obList4,obList5,obList6;
 	List<ParseObject> ob,os;
 	List<ParseUser> ou;
 	ParseUser fl = ParseUser.getCurrentUser();
@@ -79,19 +89,23 @@ public class PayedOneShopComingOrder extends Activity {
             obList2 = new ArrayList<String>();
             obList3 = new ArrayList<String>();
             obList4 = new ArrayList<String>();
+            obList5 = new ArrayList<String>();
+            obList6 = new ArrayList<String>();
             try {
             	
         	ParseQuery<ParseObject> innerQuery = ParseQuery.getQuery("Order");
         	ParseObject obj = ParseObject.createWithoutData("shop",value);
-        	innerQuery.whereEqualTo("order_status", "Payed");
+        	innerQuery.whereEqualTo("order_status", "Payed"); // Submitted == not Confirmed yet
         	innerQuery.orderByDescending("createdAt");
         	innerQuery.whereEqualTo("ShopId", obj);
         	ob = innerQuery.find();
         	for (ParseObject num : ob) {
 			obList.add(num.getParseObject("ShopId").getObjectId());
-			obList2.add(num.getParseObject("productId").getObjectId());
-			obList3.add(num.getParseObject("user_id").getObjectId());
-			obList4.add(num.getString("PaymentMethod"));
+			obList2.add(num.getParseObject("user_id").getObjectId());
+			obList3.add(num.getParseObject("productId").getObjectId());
+			obList4.add(num.getObjectId());
+			obList5.add(num.getString("payment_type"));
+			obList6.add(num.getString("order_status"));
 			}
 			} catch (ParseException e1) {
 				// TODO Auto-generated catch block
@@ -130,55 +144,66 @@ public class PayedOneShopComingOrder extends Activity {
             View listItem = convertView;
             final int pos = position;
             if (listItem == null) {
-                listItem = layoutInflater.inflate(R.layout.orders_row, null);   
+                listItem = layoutInflater.inflate(R.layout.all_orders_row, null);   
             }
             
             // Initialize the views in the layout
+             final TextView profileusername = (TextView) listItem.findViewById(R.id.textView1);
             final ImageView iv = (ImageView) listItem.findViewById(R.id.imageView1);
-            final TextView profileusername = (TextView) listItem.findViewById(R.id.textView1);
             final TextView Productname = (TextView) listItem.findViewById(R.id.textView2);
             final TextView ProductQty = (TextView) listItem.findViewById(R.id.textView3);
             final TextView TotalCost = (TextView) listItem.findViewById(R.id.textView4);
-            final TextView paymentMethod = (TextView) listItem.findViewById(R.id.textView6);
+            final TextView shopNAme = (TextView) listItem.findViewById(R.id.textView6);
+            final TextView paymentMethod = (TextView) listItem.findViewById(R.id.textView7);
+
+            Spinner orderstatues = (Spinner) listItem.findViewById(R.id.spinner1);
             
-            paymentMethod.setText("Payment Type: "+obList4.get(pos)+"");
-            
-            try{
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Ordered_Product");
-                ParseObject obj = ParseObject.createWithoutData("Product",obList2.get(pos));
-                query.whereEqualTo("product_id", obj);
-                List<ParseObject> oo = query.find();
-    			for(int i = 0; i < oo.size(); i++){
-                	int Qty = oo.get(i).getInt("product_quantity");
-                    ProductQty.setText("Quantity: "+Qty);
-                    Double QtyCostwithoutDiscount = Qty*oo.get(i).getDouble("unit_cost"); 
-                    TotalCost.setText("Cost: "+(QtyCostwithoutDiscount-(QtyCostwithoutDiscount*(oo.get(i).getDouble("product_discount")/100))));
-                }
-                            	
-                }catch(ParseException e){
-                	
-                }
+            paymentMethod.setText("Payment Type: "+obList5.get(pos)+"");
+            shopNAme.setVisibility(View.GONE);
             
             try{
-            	ParseQuery<ParseObject> query2 = ParseQuery.getQuery("Product");
-            	query2.whereEqualTo("objectId", obList2.get(pos));
-				List<ParseObject> cc = query2.find();
-				for(ParseObject m:cc){
-					Productname.setText("ProductName: "+m.get("ProductName").toString());
-				}
-            }catch(ParseException e){
-            	
-            }
-            
-            try {
-            	ParseQuery<ParseUser> query2 = ParseUser.getQuery();
-            	query2.whereEqualTo("objectId", obList3.get(pos));
-				List<ParseUser> oo = query2.find();
-				for(ParseObject m:oo){
-					profileusername.setText("@"+m.get("username").toString());
-					ParseFile fileObject = (ParseFile) m.get("ProfilePic");
-                		
-                		fileObject.getDataInBackground(new GetDataCallback() {
+                
+    			ParseQuery<ParseUser> vv = ParseUser.getQuery();
+            	vv.whereEqualTo("objectId", obList2.get(pos));
+            	List<ParseUser> oo9 = vv.find();
+    			for(int i = 0; i < oo9.size(); i++) {
+     				profileusername.setText("@"+oo9.get(i).get("username").toString()+"");
+    				
+    				ParseFile fileObject = (ParseFile) oo9.get(i).get("ProfilePic");
+            		fileObject.getDataInBackground(new GetDataCallback() {
+            			public void done(byte[] data,ParseException e) {
+            				if (e == null) {
+            					Bitmap bmp = BitmapFactory.decodeByteArray(data, 0,data.length);
+            					iv.setImageBitmap(bmp);
+            						} else {
+    							Bitmap bmp2 = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+    	    					iv.setImageBitmap(bmp2);
+            						}
+            					}
+            				});
+    				}
+    				
+            	ParseQuery<ParseObject> cc2 = ParseQuery.getQuery("Product");
+            	cc2.whereEqualTo("objectId", obList3.get(pos));
+            	List<ParseObject> oo3 = cc2.find();
+    			for(ParseObject m2:oo3){
+    				Productname.setText("Product name: "+m2.get("ProductName").toString()+"");
+    			}
+    			
+    				ParseQuery<ParseObject> j = ParseQuery.getQuery("Ordered_Product");
+    				ParseObject obj2 = ParseObject.createWithoutData("Product",obList3.get(pos));
+    				j.whereEqualTo("product_id", obj2);
+    				List<ParseObject> gg2 = j.find();
+    				
+    				for(ParseObject c2:gg2){
+    					//bb2.add(c2.getParseObject("ShopId").getObjectId());
+    					ProductQty.setText("Product Qty: "+c2.getInt("product_quantity")+"");
+    		    		TotalCost.setText("Cost: "+c2.getInt("product_quantity")*c2.getDouble("unit_cost"));
+    		    		
+    		    		/*
+        				ParseFile fileObject = (ParseFile) c2.get("product_pic");
+        				if(fileObject!=null){
+        				fileObject.getDataInBackground(new GetDataCallback() {
                 			public void done(byte[] data,ParseException e) {
                 				if (e == null) {
                 					Bitmap bmp = BitmapFactory.decodeByteArray(data, 0,data.length);
@@ -188,12 +213,54 @@ public class PayedOneShopComingOrder extends Activity {
                 						}
                 					}
                 				});
-                		
-				}
-			} catch (ParseException e1) {
+        				}
+        				*/
+
+    				}
+            }catch (ParseException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+    		
+            
+            if((obList6.get(pos)).equals("Confirmed")){
+            	
+            ArrayAdapter<CharSequence> sa = ArrayAdapter.createFromResource(getApplicationContext(),
+                    R.array.order_payed_statues, android.R.layout.simple_spinner_item);
+            sa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            orderstatues.setAdapter(sa);
+            }
+            
+            orderstatues.setOnItemSelectedListener(new OnItemSelectedListener(){
+				@Override
+				public void onItemSelected(final AdapterView<?> parent, View view,
+						final int position, long id) {
+					// TODO Auto-generated method stub
+					ParseQuery<ParseObject> query = ParseQuery.getQuery("Order");
+        	    	query.getInBackground(obList4.get(pos), new GetCallback<ParseObject>() {
+        	    		  public void done(ParseObject ord, ParseException e) {
+        	    		    if (e == null) {
+        	    		    	if(position==1){
+        	    		    		ord.put("order_status", "Delivered");
+   					    	    	ord.saveInBackground();
+   					    	    	onResume();
+    	    		    		
+    	    		    	}
+        	    		    	//onResume();
+        	    		    }
+        	    		  }
+        	    		  });
+        	    	
+					
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+            });
             
             
             return listItem;
@@ -203,5 +270,4 @@ public class PayedOneShopComingOrder extends Activity {
 
     }
 	
-
 }
